6316b4e13c1d540d35a8d53a5bf45ae3bf43465c
GabrielePrestifilippo <gabry.501@hotmail.it>
2018-03-29 13:39:54 +0200

    mock modules

--- app.js
+++ app.js
@@ -7,14 +7,23 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var statusRouter = require('./routes/status');
-var resetZeroRouter = require('./routes/resetzero');
-var setTaraRouter = require('./routes/settara');
-var readWeightRouter = require('./routes/readweight');
 
 // var Modbusclient = reqire('./mbcli.js'); // TODO export default client ...
+var mock = false
 
-var app = express();
+if (mock === true) {
+  var statusRouter = require('./mock/status');
+  var resetZeroRouter = require('./mock/resetzero');
+  var setTaraRouter = require('./mock/settara');
+  var readWeightRouter = require('./mock/readweight');
+} else {
+  var statusRouter = require('./routes/status');
+  var resetZeroRouter = require('./routes/resetzero');
+  var setTaraRouter = require('./routes/settara');
+  var readWeightRouter = require('./routes/readweight');
+}
+
+app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,7 +31,7 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -30,18 +39,18 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // mount the router on the app
-app.use('/getDatsStatus', statusRouter );
-app.use('/resetZeroDats', resetZeroRouter );
-app.use('/setTaraDats', setTaraRouter );
-app.use('/readWeightDats', readWeightRouter );
+app.use('/getDatsStatus', statusRouter);
+app.use('/resetZeroDats', resetZeroRouter);
+app.use('/setTaraDats', setTaraRouter);
+app.use('/readWeightDats', readWeightRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
